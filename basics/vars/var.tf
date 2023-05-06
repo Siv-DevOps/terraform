64d5d6069db9302e78114699e74841af4e03bc5a
Bollimuntha <i326409@MacBook-Pro-100.local>
2023-05-06 16:37:32 +0530

    1 file added

--- basics/vars/var.tf
+++ basics/vars/var.tf
@@ -9,3 +9,12 @@ output "sivs1" {
 output "sivs2" {
     value   =  "This programing is kil us ${var.siv}"
 }
+
+
+variable "siv3"{
+    default = [a,b,c,d]
+}
+
+output "siv4" {
+    value = "this is to test my name siv{var.siv3[4]}"
+}
