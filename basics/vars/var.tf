77e337ec60ee5c144bf185c6f1c794c8e8da0c4f
Bollimuntha <i326409@MacBook-Pro-100.local>
2023-05-06 16:13:29 +0530

    1 file added

--- basics/vars/var.tf
+++ basics/vars/var.tf
@@ -1,4 +1,11 @@
-variable "siv" {}
-output "siv" {
-    value   =  var.siv
+variable "siv" {
+    default       = "Devops Training"
+}
+
+output "sivs1" {
+    value     = var.siv
+}
+
+output "sivs2" {
+    value   =  "This programing is kil us ${var.siv}"
 }
