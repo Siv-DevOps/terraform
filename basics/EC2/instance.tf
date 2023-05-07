6e849892c407f8d61809b740cc472dff285f91af
Bollimuntha <i326409@MacBook-Pro-100.local>
2023-05-07 16:25:21 +0530

    siv

--- basics/EC2/instance.tf
+++ basics/EC2/instance.tf
@@ -1,8 +1,12 @@
+provider "aws" {
+  region =  us-east-1
+}
+
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami           = "ami-0c1d144c8fdd8d690"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "terraform.instance"
   }
 }
